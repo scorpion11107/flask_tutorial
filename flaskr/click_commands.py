b5eb7f1fd5edb32a989c512f9dc7358e713de454
Scorpion <soleyannp@gmail.com>
2025-05-12 16:33:35 +0100

    removed 'run-app' click command

--- flaskr/click_commands.py
+++ flaskr/click_commands.py
@@ -1,8 +1,6 @@
 import click
-from waitress import serve
 
 from . import db
-from . import app
 
 @click.command('init-db')
 def init_db_command():
@@ -10,13 +8,6 @@ def init_db_command():
     db.init_db()
     click.echo('Initialized the database.')
 
-@click.command('run-app')
-def run_app_command():
-    """Run flaskr"""
-    click.echo('Starting server...')
-    serve(app, host='0.0.0.0', port=8080)
-
 def init_app(app):
     app.teardown_appcontext(db.close_db)
     app.cli.add_command(init_db_command)
-    app.cli.add_command(run_app_command)
